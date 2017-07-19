17f9f737c04e1b8331a8ed62a123b66638226b7b
Ryan Wilson <syndicated.life@gmail.com>
2017-07-19 11:21:05 -0700

    new resourcefiles usage

--- FFXIVAPP.IPluginInterface/Properties/AssemblyInfo.cs
+++ FFXIVAPP.IPluginInterface/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("2.0.*")]
 
-[assembly: AssemblyVersion("3.0.2")]
+[assembly: AssemblyVersion("4.0.1")]
