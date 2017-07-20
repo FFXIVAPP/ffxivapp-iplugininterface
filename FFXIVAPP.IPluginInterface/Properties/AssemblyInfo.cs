6e7ee84e2f1ee1b4aad4d3dd4ceeac5fc01abcd0
Ryan Wilson <syndicated.life@gmail.com>
2017-07-19 20:56:40 -0700

    updated dependencies

--- FFXIVAPP.IPluginInterface/Properties/AssemblyInfo.cs
+++ FFXIVAPP.IPluginInterface/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("2.0.*")]
 
-[assembly: AssemblyVersion("4.0.1")]
+[assembly: AssemblyVersion("4.0.2")]
